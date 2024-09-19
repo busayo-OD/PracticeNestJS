0919aa117827acd7e9629a0f48594cbf583c74ca
Busayo Dada <toyinoluwabusayo@gmail.com>
2024-09-19 17:43:01 +0100

    User entity, module, service, dto and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { User } from "./user/entity/user.entity";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    controllers: [AppController],
+    imports: [
+        UserModule,
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: '127.0.0.1',
+            port: 3306,
+            username: 'root',
+            password: 'bus02ay94_001',
+            database: 'nestjs',
+            entities: [User],
+            synchronize: true,
+          }),
+    ],
 })
-export class AppModule {}
+export class AppModule{}
---
+++ src/user/user.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { CreateUserDto } from './dto/create-user.dto';
+import { Repository } from 'typeorm';
+import { User } from './entity/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+
+@Injectable()
+export class UserService {
+    constructor (
+        @InjectRepository(User)
+        private userRepository: Repository<User>,
+    ) {}
+
+    get(): Promise<User[]> {
+        return this.userRepository.find();
+    }
+
+    create(createUserDto: CreateUserDto) {
+        return this.userRepository.save(createUserDto);
+    }
+
+    update(updateUserDto: UpdateUserDto, userId: number) {
+        return this.userRepository.update(userId, updateUserDto);
+    }
+
+    show(id: number) {
+        return this.userRepository.findOne({ where: {id} });
+    }
+
+    delete(userId: number) {
+        return this.userRepository.delete(userId);
+    }
+}
